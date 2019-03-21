0654ae2176d1d9514dac8de5fdd2b93538a0fe01
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-03-21 19:57:56 +0100

    added Toolbar;

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../hoc/Auxiliary';
 import classes from './Layout.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => {
-    return (
-    <Aux>
-        <div>Toolbar, Sidebar, Backdrop</div>
-        <main className={classes.Content}>{props.children}</main>
-    </Aux>
-    )
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+    sideDrawerCloseHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+    sideDrawerOpenHandler = () => {
+        this.setState({showSideDrawer: true})
+    }
+
+    render() {
+        return (
+        <Aux>
+            <Toolbar opend={this.sideDrawerOpenHandler}/>
+            <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+            <main className={classes.Content}>{this.props.children}</main>
+        </Aux>
+        )
+    }
 }
 
-export default layout;
+export default Layout;
