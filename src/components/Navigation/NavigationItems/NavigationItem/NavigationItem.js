e252db95d40ac34e9f479c15582db7f860057a17
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-03-25 23:17:57 +0100

    routing added;

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
 import classes from './NavigationItem.css'
 
 const navigationItem = (props) => {
   return (
     <li className={classes.NavigationItem}>
-        <a 
-            href={props.link} 
-            className={props.active ? classes.active : null}>
-                {props.children}
-        </a>
+        <NavLink 
+          to={props.link}
+          exact
+          activeClassName={classes.active}
+        >
+            {props.children}
+        </NavLink>
     </li>
   )
 }
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,15 +1,20 @@
 import React, { Component } from 'react';
 import Layout from '../../hoc/Layout/Layout';
 import BurgerBuilder from '../BurgerBuilder/BurgerBuilder';
+import Checkout from '../Checkout/Checkout';
+import Orders from '../Orders/Orders';
+import {Route, Switch} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Layout>
-          <BurgerBuilder/>
-        </Layout>
-      </div>
+      <Layout>
+        <Switch>
+          <Route path='/checkout' component={Checkout} />
+          <Route path='/orders' component={Orders} />
+          <Route path='/' component={BurgerBuilder} />
+        </Switch>
+      </Layout>
     );
   }
 }
