06dfd3337eb6bee5a29fd568dae373b9a98aa98e
Zuzanna Pusiewicz <zuzpusie@student.pg.gda.pl>
2019-05-18 22:15:56 +0200

    guard routes

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -6,7 +6,7 @@ import Checkout from '../Checkout/Checkout';
 import Orders from '../Orders/Orders';
 import Logout from '../Auth/Logout/Logout';
 import Auth from '../Auth/Auth';
-import {Route, Switch, withRouter} from 'react-router-dom';
+import {Route, Switch, withRouter, Redirect} from 'react-router-dom';
 import * as actions from '../../store/actions/index';
 
 class App extends Component {
@@ -15,17 +15,37 @@ class App extends Component {
   }
 
   render() {
-    return (
-      <Layout>
+    let routes = (
+      <Switch>
+        <Route path='/auth' component={Auth} />
+        <Route path='/' exact component={BurgerBuilder} />
+        <Redirect to='/'/>
+      </Switch>
+    );
+
+    if(this.props.isAuthenticated) {
+      routes = (
         <Switch>
           <Route path='/checkout' component={Checkout} />
           <Route path='/orders' component={Orders} />
-          <Route path='/auth' component={Auth} />
           <Route path='/logout' component={Logout} />
-          <Route path='/' component={BurgerBuilder} />
+          <Route path='/' exact component={BurgerBuilder} />
+          <Redirect to='/'/>
         </Switch>
+      )
+    }
+
+    return (
+      <Layout>
+        {routes}
       </Layout>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null
   }
 }
 
